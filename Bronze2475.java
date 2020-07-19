a0a91f4a37bc31fbddad85b8e16e1d845eb7d9ec
Kuangjugaza <yoon2449@naver.com>
2020-07-20 00:49:07 +0800

    7.19

---
+++ Bronze2475.java
@@ -0,0 +1,22 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class Bronze2475 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+		int d = scanner.nextInt();
+		int e = scanner.nextInt();
+
+		System.out.println((a^2+b^2+c^2+d^2+e^2)/10);
+		
+		
+	}
+
+}
