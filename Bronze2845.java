2ca4bbc34840c566bac4fd91a2b463711dd2af6d
Kuangjugaza <yoon2449@naver.com>
2020-07-21 00:01:19 +0800

    After party

---
+++ Bronze2845.java
@@ -0,0 +1,26 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class Bronze2845 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+		int d = scanner.nextInt();
+		int e = scanner.nextInt();
+		int f = scanner.nextInt();
+		int g = scanner.nextInt();
+
+		System.out.println((c - (a*b)) + " " + (d - (a*b)) + " " + (e - (a*b)) + " " + (f - (a*b)) + " " + (g - (a*b)));
+		
+
+		
+		
+	}
+
+}
