b25b3ab9aaf9275b988144d8b8ebe2b5f6458e72
Kuangjugaza <yoon2449@naver.com>
2020-07-22 01:03:00 +0800

    Bronze8393

---
+++ Iplus.java
@@ -0,0 +1,23 @@
+package bronze5;
+
+public class Iplus {
+
+	public static void main(String[] args) {
+
+		int i = 10;
+		
+		
+		
+		
+		
+		System.out.println(++i);
+		System.out.println(i);
+
+		
+		
+	}
+
+
+	
+}
+
