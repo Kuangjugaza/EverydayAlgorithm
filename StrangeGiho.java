1cbe256e8df673ae5be4cc7564b6cbc08fed205c
Kuangjugaza <yoon2449@naver.com>
2020-08-04 00:07:36 +0800

    20.08.04.01:07

---
+++ StrangeGiho.java
@@ -0,0 +1,24 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class StrangeGiho {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Scanner scanner = new Scanner(System.in);
+
+		long a = scanner.nextLong();
+		long b = scanner.nextLong();
+		
+		System.out.println((a+b)*(a-b));
+
+	}
+
+		
+		
+		
+		
+}	
